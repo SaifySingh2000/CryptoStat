9041f9004d25acf47eaca5601a6ef6c42b188781
SaifySingh2000 <59423339+SaifySingh2000@users.noreply.github.com>
2021-12-10 01:16:41 -0600

    Form Validation
    This form ensures that all the fields in it are correctly filled. If any of the credentials are not correct or are left unfilled, a warning message pops up.

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php 
+session_start();
+
+// connect to database
+$db = mysqli_connect('localhost', 'root', '', 'cryptocurrency');
+
+// variable declaration
+$username = "";
+$email    = "";
+$errors   = array(); 
+
+// call the register() function if register_btn is clicked
+if (isset($_POST['register_btn'])) {
+	register();
+}
+
+// REGISTER USER
+function register(){
+	// call these variables with the global keyword to make them available in function
+	global $db, $errors, $username, $email;
+
+	// receive all input values from the form. Call the e() function
+    // defined below to escape form values
+	$username    =  e($_POST['username']);
+	$email       =  e($_POST['email']);
+	$password_1  =  e($_POST['password_1']);
+	$password_2  =  e($_POST['password_2']);
+
+	// form validation: ensure that the form is correctly filled
+	if (empty($username)) { 
+		array_push($errors, "Username is required"); 
+	}
+	if (empty($email)) { 
+		array_push($errors, "Email is required"); 
+	}
+	if (empty($password_1)) { 
+		array_push($errors, "Password is required"); 
+	}
+	if ($password_1 != $password_2) {
+		array_push($errors, "The two passwords do not match");
+	}
+  
+  ?>
