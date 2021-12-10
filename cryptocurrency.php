90e183aebd3c22be31f23beb054f2e2ff12d7b4c
SaifySingh2000 <59423339+SaifySingh2000@users.noreply.github.com>
2021-12-10 01:26:25 -0600

    Created Search Form For Cryptostat
    The following HTML code helps us to get the desired Cryptocurrency by searching for it using specific keywords or the name of the currency itself. The name along with its associated data appears as a result. The search result for crypto will display in the table.

--- cryptocurrency.php
+++ cryptocurrency.php
@@ -56,3 +56,157 @@
     $row = $fetch_statement->fetchAll();
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home</title>
+	
+	<link rel="stylesheet" type="text/css" href="user_header.css">
+	<style>
+		/* general styling */
+		body {
+		font-family: "Open Sans", sans-serif;
+		line-height: 1.25;
+		}
+
+		table {
+		border: 1px solid #ccc;
+		border-collapse: collapse;
+		margin: 10px;
+		padding: 0;
+		width: 100%;
+		table-layout: fixed;
+		}
+
+		table caption {
+		font-size: 1.5em;
+		margin: .5em 0 .75em;
+		}
+
+		table tr {
+		background-color: #f8f8f8;
+		border: 1px solid #ddd;
+		padding: .35em;
+		}
+
+		table th,
+		table td {
+		padding: .625em;
+		text-align: center;
+		word-wrap: break-word;
+		}
+
+		table th {
+		font-size: .85em;
+		letter-spacing: .1em;
+		text-transform: uppercase;
+		}
+
+		footer{
+			font-size: 20px;
+			text-align: center;
+			color: #b6b7bf;
+		}
+
+		@media screen and (max-width: 600px) {
+			table {
+				border: 0;
+			}
+
+			table caption {
+				font-size: 1.3em;
+			}
+			
+			table thead {
+				border: none;
+				clip: rect(0 0 0 0);
+				height: 1px;
+				margin: -1px;
+				overflow: hidden;
+				padding: 0;
+				position: absolute;
+				width: 1px;
+			}
+			
+			table tr {
+				border-bottom: 3px solid #ddd;
+				display: block;
+				margin-bottom: .625em;
+			}
+			
+			table td {
+				border-bottom: 1px solid #ddd;
+				display: block;
+				font-size: .8em;
+				text-align: right;
+			}
+			
+			table td::before {
+				/*
+				* aria-label has no advantage, it won't be read inside a table
+				content: attr(aria-label);
+				*/
+				content: attr(data-label);
+				float: left;
+				font-weight: bold;
+				text-transform: uppercase;
+			}
+			
+			table td:last-child {
+				border-bottom: 0;
+			}
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<?php include('user_header.php') ?>
+	</header>
+    <form action="" method="POST">
+		<div>
+			<label>Search the cryptocurrency</label>
+        <input id="searchBox" type="text" name='search[keyword]' value="<?php echo $search_keyword; ?>" id='keyword' maxlength='25' placeholder="Search Here..." >
+
+        <div class="limiter">
+			<div class="container-table100">
+				<div class="wrap-table100">
+					<div class="table100">
+						<table class="responsive-table">
+							<thead>
+								<tr class="table100-head">
+									<th class="column1">Crytpocurrency</th>
+									<th></th>
+									<th class="column2">Rank</th>
+									<th class="column3">Symbol</th>
+									<th class="column4">Supply</th>
+									<th class="column5">Maximum Supply</th>
+									<th class="column6">Website</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach($row as $data) : ?>
+									<tr class="table-row">
+										<td class="column1"><?= $data['cryptocurrency'] ?><td>
+										<td class="column2"><?= $data['rank'] ?></td>
+										<td class="column3" style="text-tranform: uppercase;"><?= $data['symbol'] ?></td>
+										<td class="column4"><?= $data['supply'] ?></td>
+										<td class="column5"><?= $data['maximum_supply'] ?></td>
+										<td class="column6"><a href="<?= $data['website'] ?>"><?= $data['website'] ?></a></td>
+									</tr>
+								<?php endforeach ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php echo $per_page_html; ?>
+		</form>
+	<footer>
+			<small>Copyright @<?= date('Y') ?> - Saify Inc. - All rights reserved</small>
+	</footer>
+</body>
+</html>
